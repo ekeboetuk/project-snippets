b2540448bed3181dd0ebf1a5d5645e6b9be0d052
Ekebo Etuk <ekebo.etuk@gmail.com>
2023-02-23 14:57:52 +0100

    more contents addition and code modifications

---
+++ web-app/typescript.ts
@@ -0,0 +1,13 @@
+let person: {
+    name: string;
+    age: number;
+    isEmployee: boolean
+};
+
+person = {
+    name: 'Ekebo Etuk',
+    age: 37,
+    isEmployee: true
+}
+
+console.log(person.isEmployee)
